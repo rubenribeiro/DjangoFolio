3f382200160e3594e89eef0dc554f995e7b08522
Ruben Ribeiro <rubenribeiro@outlook.com>
2018-03-28 22:48:46 -0400

    Added bootstrap and completed the Jobs App

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=1255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to="images/blog/")
+
+    def __str__(self):
+        return self.title
