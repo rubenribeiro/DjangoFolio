5ba0291bc47e53e01ca3723dda113cb05855d3cd
Ruben Ribeiro <rubenribeiro@outlook.com>
2018-03-29 00:09:54 -0400

    Added blog posts and details

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Blog
 
 # Create your views here.
+
+def allblogs(request):
+    posts = Blog.objects
+    return render(request, 'blog/allblogs.html', {'posts': posts})
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    print(detailblog)
+    return render(request, 'blog/detail.html', {'blog': detailblog})
+
